f6d958f79df6f33033c2fb14c0d61000dcf2f45d
Tateishi Manaka <monakanomannaka.721@gmail.com>
2021-10-24 13:55:56 +0000

    public order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,18 @@
 class Public::OrdersController < ApplicationController
-  def new  
-    @order = Order.new(params[:id])
+  def new
+    @order = Order.new
   end
-  
+
   def comfilm
-    
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.total_price = 0
+    @order.postage = 800
+    @order.order_staus = "waiting_for_payment"
   end
-  
-  
+
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :postage, :total_price, :payment_method, :order_status, :customer_id)
+  end
 end
