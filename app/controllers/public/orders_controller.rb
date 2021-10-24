09da7683a07e2e45c10efe55a3bf93d0347e162f
Tateishi Manaka <monakanomannaka.721@gmail.com>
2021-10-24 14:44:39 +0000

    public order.address_1

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,7 +8,18 @@ class Public::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
     @order.total_price = 0
     @order.postage = 800
-    @order.order_staus = "waiting_for_payment"
+    @order.order_status = "waiting_for_payment"
+    if params[:order][:address_option] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+      @order.postal_code = Address.find
+    else params[:order][:address_option] == "2"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
   end
 
   private
