9522038edea20416b9e228eeed373b4a00b6b1dd
Tateishi Manaka <monakanomannaka.721@gmail.com>
2021-10-24 03:02:40 +0000

    pubulic itemshow

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,13 @@
 class Public::CartItemsController < ApplicationController
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to  cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:amount, :item_id)
+  end
 end
