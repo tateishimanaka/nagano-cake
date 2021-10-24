ea1d22a177d5fea9a75ed1591b5b8b9db46c8fde
Tateishi Manaka <monakanomannaka.721@gmail.com>
2021-10-24 01:46:24 +0000

    pubulic item index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,12 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @get_items = Item.where(is_active: true)
+    @items = @get_items.page(params[:page]).per(10)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 end
