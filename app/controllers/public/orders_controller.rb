8a407a8fa33e06e8f00e5fb47cb349cd550505bf
Tateishi Manaka <monakanomannaka.721@gmail.com>
2021-10-28 13:51:37 +0000

    public order show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -54,6 +54,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   private
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,10 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  
+
+  def subtotal
+    item.add_tax_price * amount
+  end
+
   enum order_item_status: { impossible: 0, stay: 1, make: 2, finish: 3 }
 end
- 
