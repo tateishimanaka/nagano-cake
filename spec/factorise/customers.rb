c0081de0b033c30662a29fc86f6bebace7ed074c
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-01 12:53:14 +0000

    factory-customer.rb

---
+++ spec/factorise/customers.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :customer do
+    email { Faker::Internet.email }
+    password { 'password' }
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    last_name_kana { Faker::Name.last_name_kana }
+    first_name_kana { Faker::Name.first_name_kana }
+    postal_code { Faker::Address.postcode }
+    address { Faker::Address.full_address }
+    telephone_number { Faker::Number.number }
+    is_deleted { Faker::Boolean.boolean(true_ratio: 0.2) }
+  end
+end
