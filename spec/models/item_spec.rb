2771681f185205da15db24f13b39aa3e13afef4b
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-11 07:40:26 +0000

    spec-item.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 Rspec.describe Item, "Itemモデルのテスト", type: :model do
-  
+  describe '実際に保存してみる' do
+    it "有効な投稿内容の場合は保存されるのか" do
+      expect(FactoryBot.build(:item)).to be_valid
+    end
+  end
 end
