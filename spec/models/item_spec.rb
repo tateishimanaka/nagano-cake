b4910e55adb40710bc7017a4838c690cc25ecbe3
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-14 13:42:17 +0000

    spec-item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,4 +8,11 @@ Rspec.describe Item, "Itemモデルのテスト", type: :model do
       expect(FactoryBot.build(:item)).to be_valid
     end
   end
+  context "空白のバリデーションチェック" do
+    it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+      item = Item.new(name: '', introduction: 'hoge', price: 'hoge')
+      expect(item).to be_invalid
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+  end
 end
