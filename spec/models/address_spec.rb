bf23ce8a23dc67b245bfdfd17b618fccc942f986
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-16 13:45:02 +0000

    address.rb

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+Rspec.describe Address, "Addressモデルのテスト", type: :model do
+  describe '実際に保存してみる' do
+    it "有効な投稿内容の場合は保存されるのか" do
+      expect(FactoryBot.build(:address)).to be_valid
+    end
+  end
+end
