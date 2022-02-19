2f2f63bb688081789ca7bee04924cfc6bdadbef1
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-19 06:18:55 +0000

    spec-model-customer.spec.rb

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,4 +2,10 @@
 
 require 'rails_helper'
 
-Rspec
+Rspec.describe Customer, "Customerモデルのテスト", type: :model do
+  describe '実際に保存してみる' do
+    it "有効な会員の場合は保存されるのか" do
+      expect(FactoryBot.build(:customer)).to be_valid
+    end
+  end
+end
