7d9fd6ccfdf63b0574898c51038b60b7aebaabdd
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-19 07:27:39 +0000

    spec-model-order_item.spec.rb

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+Rspec.describe OrderItem, "OrderItemモデルのテスト", type: :model do
+  describe '実際に保存してみる' do
+    it "有効な内容の場合保存されるのか" do
+      expect(FactoryBot.build(:order_item)).to be_valid
+    end
+  end
+end
