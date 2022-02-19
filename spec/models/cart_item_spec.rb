995986c7aab174dfecc90255f0bb8f27be0d3c3d
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-19 07:14:22 +0000

    spec-model-cart_item.spec.rb

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+Rspec.describe CartItem, "CartItemモデルのテスト", type: :model do
+  describe '実際に保存してみる' do
+    it "有効な内容の場合保存されるのか" do
+      expext(FactoryBot.build(:cart_item)).to be_valid
+    end
+  end
+end
