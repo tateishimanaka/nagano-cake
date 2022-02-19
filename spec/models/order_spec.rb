5d698bab991649108e8fa3239295b4096381784a
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-19 07:30:12 +0000

    spec-model-order.spec.rb

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+Rspec.describe Order, "Orderモデルのテスト", type: :model do
+  describe '実査に保存する' do
+    it "有効な内容の場合保存されるか" do
+      expect(FactoryBot.build(:order)).to be_valid
+    end
+  end
+end
