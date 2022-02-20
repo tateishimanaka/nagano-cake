d98ba672c50bb251a7bdaa7031ff381174bc3912
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-20 07:09:46 +0000

    spec-model-item.spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,17 +2,59 @@
 
 require 'rails_helper'
 
-Rspec.describe Item, "Itemモデルのテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な投稿内容の場合は保存されるのか" do
-      expect(FactoryBot.build(:item)).to be_valid
+Rspec.describe 'Itemモデルのテスト', type: :model do
+  describe  'バリデーションのテスト' do
+    subject { item.valid? }
+
+    let(:genre) { create(:genre) }
+    let!(:item) { build(:item, genre_id: genre.id) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        item.name = ''
+        is_expexted.to eq false
+      end
+    end
+
+    context 'imageカラム' do
+      it '空欄でないこと' do
+        item.image = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'introductionカラム' do
+      it '空欄でないこと' do
+        item.introduction = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'priceカラム' do
+      it '空欄でないこと' do
+        item.price = ''
+        is_expected.to eq false
+      end
     end
   end
-  # context "空白のバリデーションチェック" do
-  #   it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-  #     item = Item.new(name: '', introduction: 'hoge', price: 'hoge')
-  #     expect(item).to be_invalid
-  #     expect(item.errors[:name]).to include("can't be blank")
-  #   end
-  # end
+
+  describe 'アソシエーションのテスト' do
+    context 'genreモデルとの関係性' do
+      it 'N:1の関係性になっている' do
+        expect(Item.reflect_on_association(:genre).macro).to eq :belongs_to
+      end
+    end
+
+    context 'cart_itemモデルとの関係性' do
+      it '1:Nの関係性になっている' do
+        expect(Item.reflect_on_association(:cart_items).macro).to eq :has_many
+      end
+    end
+
+    context 'order_itemモデルとの関係性' do
+      it '1:Nの関係性になっているか' do
+        expect(Item.reflect_on_association(:order_items).macro).to eq :has_many
+      end
+    end
+  end
 end
