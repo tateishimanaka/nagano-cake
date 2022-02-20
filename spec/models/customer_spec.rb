c49d49b7a799e18c75d5f69ea54bb11c14f0e9b1
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-20 08:15:08 +0000

    spec-model-address.spec.rb

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,40 @@
 
 require 'rails_helper'
 
-Rspec.describe Customer, "Customerモデルのテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な会員の場合は保存されるのか" do
-      expect(FactoryBot.build(:customer)).to be_valid
+Rspec.describe 'addressモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { address.valid? }
+
+    let(:customer) { create(:customer) }
+    let!(:address) { build(:address, customer_id: customer.id) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        address.name = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'postal_codeカラム' do
+      it '空欄でないこと' do
+        address.postal_code = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'addressカラム' do
+      it '空欄でないこと' do
+        address.address = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'customerモデルとの関係性' do
+      it 'N:1の関係性になっている' do
+        expect(Address.reflect_on_association(:customer).macro).to eq :belongs_to
+      end
     end
   end
 end
