82d4aaaaf1cae5be0cc55b40160e9f479c944ef1
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-20 13:15:18 +0000

    spec-model-genre.spec.rb

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -2,10 +2,25 @@
 
 require 'rails_helper'
 
-Rspec.describe Genre, "Genreモデルのテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な投稿内容の場合は保存されるのか" do
-      expect(FactryBot.build(:genre)).to be_valid
+Rspec.describe 'Genreモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { genre.valid? }
+
+    let(:genre) { build(:genre) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        genre.name = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Itemモデルとの関係性' do
+      it '1:Nの関係性になっているか' do
+        expect(Genre.reflect_on_association(:items).macro).to eq :has_many
+      end
     end
   end
 end
