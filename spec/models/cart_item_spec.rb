d942e0bb6e996b4204d1fa66aa5e10876369a343
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-20 13:46:26 +0000

    spec-model-cart_item.spec.rb

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -2,10 +2,33 @@
 
 require 'rails_helper'
 
-Rspec.describe CartItem, "CartItemモデルのテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な内容の場合保存されるのか" do
-      expext(FactoryBot.build(:cart_item)).to be_valid
+Rspec.describe 'CartItemモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { cart_item.valid? }
+
+    let(:customer) { create(:customer) }
+    let(:item) { create(:item) }
+    let!(:cart_item) { build(:cart_item, customer_id: customer.id, item_id: item.id ) }
+
+    contxt 'amountカラム' do
+      it '空欄でないこと' do
+        cart_item.amount = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Customerモデルとの関係性' do
+      it 'N:1の関係性になっているか' do
+        expect(CartItem.reflect_on_association(:customer).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Itemモデルとの関係性' do
+      it 'N:1の関係性になっているか' do
+        expect(CartItem.reflect_on_association(:item).macro).to eq :belongs_to
+      end
     end
   end
 end
