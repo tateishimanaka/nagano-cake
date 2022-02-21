41da86a0b143144b577181044ff1138868c541f1
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-21 13:33:48 +0000

    spec-model-order_item.spec.rb

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -2,10 +2,18 @@
 
 require 'rails_helper'
 
-Rspec.describe OrderItem, "OrderItemモデルのテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な内容の場合保存されるのか" do
-      expect(FactoryBot.build(:order_item)).to be_valid
+Rspec.describe 'OrderItemモデルのテスト', type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'Itemモデルとの関係性' do
+      it 'N:1の関係性になっているか' do
+        expect(OrderItem.reflect_on_association(:item).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Orderモデルとの関係性' do
+      it 'N:1の関係性になっているか' do
+        expect(OrderItem.reflect_on_association(:order).macro).to eq :belongs_to
+      end
     end
   end
 end
