c33f9cd88e5b8c79e77d739d97395891768bd2d9
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-02-21 13:20:31 +0000

    spec-model-order.spec.rb

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,10 +2,53 @@
 
 require 'rails_helper'
 
-Rspec.describe Order, "Orderモデルのテスト", type: :model do
-  describe '実査に保存する' do
-    it "有効な内容の場合保存されるか" do
-      expect(FactoryBot.build(:order)).to be_valid
+Rspec.describe 'Orderモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { order.valid? }
+
+    let(:customer) { create(:customer) }
+    let!(:order) { build(:order, custoemr_id: customer.id) }
+
+    context 'postal_codeカラム' do
+      it '空欄でないこと' do
+        order.postal_code = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'addressカラム' do
+      it '空欄でないこと' do
+        order.address = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        order.name = ''
+        is_expected.to eq false
+      end
+    end
+
+    context 'payment_methodカラム' do
+      it '空欄でないこと' do
+        order.payment_method = ''
+        is_expected.to eq false
+      end
+    end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Customerモデルとの関係性' do
+      it 'N:1の関係性になっていること' do
+        expect(Order.reflect_on_association(:customer).macro).to eq :belongs_to
+      end
+    end
+
+    context 'OrderItemモデルとの関係性' do
+      it '1:Nの関係性になっていること' do
+        expect(Order.reflect_on_association(:order_items).macro).to eq :has_many
+      end
     end
   end
 end
