d855c778a481ac7b01b184353999baeaf37fe5a0
Tateishi Manaka <monakanomannaka.721@gmail.com>
2022-03-21 15:10:11 +0000

    spec-system-sign_up_link

--- spec/system/01_customer_before_login_spec.rb
+++ spec/system/01_customer_before_login_spec.rb
@@ -12,9 +12,24 @@ describe '[STEP1]customerログイン前のテスト' do
       it 'URLが正しい' do
         expect(current_path).to eq '/'
       end
-      it 'ログインリンクが表示される' do
+      it 'ログインのリンクが表示される' do
         log_in_link = find_all('a')[4].native.inner_text
         expect(log_in_link).to match "ログイン"
+      end
+
+      it 'ログインリンクの内容が正しい' do
+        log_in_link = find_all('a')[4].native.inner_text
+        expect(page).to have_link log_in_link, href: new_customer_session_path
+      end
+
+      it '新規登録のリンクが表示される' do
+        sign_up_link = find_all('a')[3].native.inner_text
+        expect(sign_up_link).to match "新規登録"
+      end
+
+      it '新規登録の内容が正しい' do
+        sign_up_link = find_all('a')[3].native.inner_text
+        expect(page).to have_link sign_up_link, href: new_customer_registration_path
       end
     end
   end
